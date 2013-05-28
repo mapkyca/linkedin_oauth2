mapkyca/linkedin_oauth2

commit 1edd7d1cfbc0556dda26869b848c1c744648fd43

Tweaks

authenticate.php
@@ -18,7 +18,7 @@
     if (!get_input('code'))
     {
         $params = array('scope' => 'r_fullprofile r_emailaddress');
-        $params = elgg_trigger_plugin_hook('linkedin_oauth2', 'permissions', null, $params); // Allow customisation permissions
+        $params = elgg_trigger_plugin_hook('linkedin_oauth2', 'permissions', $params); // Allow customisation permissions
         $auth_url = $client->getAuthenticationUrl($AUTHORIZATION_ENDPOINT . "?response_type=code&state=$state", $REDIRECT_URI, $params);
         header('Location: ' . $auth_url);
         die('Redirect');

commit 56999d6d31f2ebb3c07ed64256b5d47f0eb4af45

Fixing regression introduced in 1edd7d1cfbc0556dda26869b848c1c744648fd43

authenticate.php
@@ -21,7 +21,7 @@
     if (!get_input('code'))
     {
         $params = array('scope' => 'r_fullprofile r_emailaddress');
-        $params = elgg_trigger_plugin_hook('linkedin_oauth2', 'permissions', $params); // Allow customisation permissions
+        $params = elgg_trigger_plugin_hook('linkedin_oauth2', 'permissions', null, $params); // Allow customisation permissions
         $auth_url = $client->getAuthenticationUrl($AUTHORIZATION_ENDPOINT . "?response_type=code&state=$state", $REDIRECT_URI, $params);
         header('Location: ' . $auth_url);
         die('Redirect');